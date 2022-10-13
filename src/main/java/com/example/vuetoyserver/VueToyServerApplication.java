22cdec72afc76511b446ca4a6aba2253dcec5c7c
hnuikim-mn <cocacola2017@naver.com>
2022-10-14 00:36:53 +0900

    init project

---
+++ src/main/java/com/example/vuetoyserver/VueToyServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.vuetoyserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VueToyServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VueToyServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/vuetoyserver/VueToyServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.vuetoyserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VueToyServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
